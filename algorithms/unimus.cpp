02d25c4868537edab132c9191753288cb271a2cd
jar-ben <xbendik@gmail.com>
2020-02-20 14:35:38 +0100

    Working, but not so efficient yet

--- algorithms/unimus.cpp
+++ algorithms/unimus.cpp
@@ -33,58 +33,69 @@ bool Master::unimus_hitting_pair(int mid1, int mid2, int c1, int c2){
 void Master::unimus_add_blocks(MUS &m1, int from, int to, vector<vector<int>> &blocks){
 	for(int i = from; i <= to; i++){
 		//todo try to add i as the first element and then use c1 in the unex check below
-		vector<int> block;
+		vector<int> block(1, i);
 		for(auto c: muses[i].int_mus){
 			if(!m1.bool_mus[c]){
 				block.push_back(c);
 			}
 		}
-		blocks.push_back(block);
+		if(block.size() > 1)
+			blocks.push_back(block);
 	}
 }
 
 void Master::unimus_rotate_mus(int mid){
 	MUS m1 = muses[mid];
 	vector<vector<int>> blocks;
-	unimus_add_blocks(m1, 0, mid - 1, blocks);
+	unimus_add_blocks(m1, 0, muses.size() - 1, blocks);
 	//for(int i = mid - 10; i < mid; i++){
-	for(int i = mid - 10; i < mid; i++){
+	for(int i = mid - 40; i < mid; i++){
 		if(i < 0) continue;
-		MUS m2 = muses[i];
+		if(abs(int(m1.int_mus.size() - muses[i].int_mus.size())) > 200) continue;
 		vector<int> m1_over;
 		for(auto c1: m1.int_mus)
-			if(!m2.bool_mus[c1])
+			if(!muses[i].bool_mus[c1])
 				m1_over.push_back(c1);
 		vector<int> m2_over;
-		for(auto c2: m2.int_mus)
+		for(auto c2: muses[i].int_mus)
 			if(!m1.bool_mus[c2])
 				m2_over.push_back(c2);
-		if(m1_over.size() * m2_over.size() > 100) continue;
+//		if(m1_over.size() * m2_over.size() > 100) continue;
 		bool found = false;
-		vector<Formula> candidates;
+		bool foundSeed = false;
+		int iter = 0;
 		for(auto c1: m1_over){
 			for(auto c2: m2_over){
+				iter++;
+				if(!unimus_hitting_pair(mid, i, c1, c2))
+					continue;
+				unimus_attempts++;
 				//check if the seed is unexplored
 				bool ok = true;
 				for(auto &block: blocks){
-					ok = false;
-					for(auto c: block){
-						ok = ok || c == c2 || !m2.bool_mus[c];
+					int musId = block[0];
+					ok = muses[musId].bool_mus[c1];
+					for(int k = 1; k < block.size(); k++){
+					       	int c = block[k];	
+						ok = ok || c == c2 || !muses[i].bool_mus[c];
 					}
 					if(!ok) break;
 				}
 				if(!ok) continue;
-				unimus_attempts++;
-				if(unimus_hitting_pair(mid, i, c1, c2)){
-					Formula seed = union_sets(m1.bool_mus, m2.int_mus);
+				//if(unimus_hitting_pair(mid, i, c1, c2)){
+					Formula seed = union_sets(m1.bool_mus, muses[i].int_mus);
 					seed[c1] = seed[c2] = false;
 					unimus_rotated++;
 					MUS mus = shrink_formula(seed);					
 					mark_MUS(mus);
 					unimus_add_blocks(m1, muses.size() - 1, muses.size() - 1, blocks);
 					unimus_rotation_queue.push(muses.size() - 1);
-				}
+					foundSeed = true;
+					cout << "banana " << (float(iter)/(m1_over.size() * m2_over.size())) << endl;
+					break;
+					//}
 			}
+			if(foundSeed) break;
 		}
 	}
 }
